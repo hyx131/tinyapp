d8d2a014432b022a32ec2d026658d501c76b07a4
Selin Huang <selin.hyx@gmail.com>
2019-07-08 21:11:20 +0000

    installed middleware body-parser library and inspected the request body content as js object

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -26,10 +29,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_Index", { urls: urlDatabase });
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
